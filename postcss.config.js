760c1c08fcf29785c91f0466a1f7f2a18040c225
Murray <Murraydmac@gmail.com>
2020-04-28 16:51:08 +0200

    Whitlested carousel styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    'vue-cli-plugin-tailwind/purgecss': {}
-  }
-}
+    plugins: {
+        tailwindcss: {},
+        "vue-cli-plugin-tailwind/purgecss": {
+            whitelistPatterns: [
+                /(^|\.)ra/,
+                /(^|\.)lg/,
+                /(^|\.)fa-/,
+                /-fa($|\.)/,
+                /(^|\.)fas-/,
+                /-fas($|\.)/,
+                /(^|\.)slick-/,
+                /-bg2($|\.)/
+            ]
+        }
+    }
+};
