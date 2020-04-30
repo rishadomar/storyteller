e8e8fa3b5a3ca219eec2fdc7bf8bfe12c2654467
Rishad Omar <rishad@rhinoafrica.com>
2020-04-30 15:06:20 +0200

    Trying to solve deploy issue where carousel shows all images vertically.

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,17 @@
+const IN_PRODUCTION = process.env.NODE_ENV === 'production'
+
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        "vue-cli-plugin-tailwind/purgecss": {}
-    },
-    purgeCSS: {
-        // your settings here
-        whitelistPatterns: [
-            /(^|\.)ra/,
-            /(^|\.)lg/,
-            /(^|\.)fa-/,
-            /-fa($|\.)/,
-            /(^|\.)fas-/,
-            /-fas($|\.)/,
-            /(^|\.)slick-/,
-            /-bg2($|\.)/
-        ]
-    }
-};
+  plugins: [
+    require('tailwindcss')(),
+    IN_PRODUCTION && require('@fullhuman/postcss-purgecss')({
+      content: [ `./public/**/*.html`, `./src/**/*.vue` ],
+      defaultExtractor (content) {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+      },
+      whitelist: [],
+      whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/ ],
+    }),
+    require('autoprefixer')(),
+  ],
+}
