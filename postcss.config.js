3e15bde7d2401e6390958cd5ee7e3d3ae50c3024
Rishad Omar <rishad@rhinoafrica.com>
2020-04-30 15:27:14 +0200

    Further attempts at fixing the carousel

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,19 @@
-const IN_PRODUCTION = process.env.NODE_ENV === 'production'
-
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const purgecss = require("@fullhuman/postcss-purgecss");
 module.exports = {
-  plugins: [
-    require('tailwindcss')(),
-    IN_PRODUCTION && require('@fullhuman/postcss-purgecss')({
-      content: [ `./public/**/*.html`, `./src/**/*.vue` ],
-      defaultExtractor (content) {
-        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
-        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
-      },
-      whitelist: [],
-      whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/ ],
-    }),
-    require('autoprefixer')(),
-  ],
-}
+    plugins: [
+        tailwindcss("./tailwind.config.js"),
+        autoprefixer({
+            add: true,
+            grid: true
+        }),
+        //Only add purgecss in production
+        process.env.NODE_ENV === "production"? purgecss({
+            content: [
+                "./src/**/*.html",
+                "./src/**/*.vue"
+            ]
+        }): ""
+    ]
+};
